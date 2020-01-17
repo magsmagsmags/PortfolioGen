226d42af72521534958c8d2e74e4a7fdb68afc70
Maggie Kiefer <57575667+magsmagsmags@users.noreply.github.com>
2020-01-17 10:03:21 -0600

    index.js reorganized and consts added

--- index.js
+++ index.js
@@ -4,12 +4,26 @@
 // generator
 const inquirer = require("inquirer");
 const fs = require("fs");
-const axios = require('axios').default;
-// const db = require("db");
-// const open = require('open');
+const axios = require("axios");
 
 const HTMLToPDF = require("html-pdf");
 const generate = require("./generateHTML.js");
+
+let profileImg;
+let githubUsername;
+let userCity;
+let userGithubProfileURL;
+let userBlogURL;
+let userBio;
+let numberOfRepos;
+let numberofFollowers;
+let numberofUsersFollowing;
+let userCompany;
+
+
+//========= Inquirer prompts the user for Github username and favorite color.
+
+
 inquirer.prompt([{
             type: "input",
             name: "name",
@@ -26,7 +40,28 @@ inquirer.prompt([{
                 "red"
             ]
         }
-    ]).then(function (res) {
+    ])
+    .then(function (res) {
             console.log(res.name, res.color);
             const userName = res.name;
             const userColor = res.color;
+
+            .then(function (res) {
+                        // we create a json file with that information.
+                        var githubUsername = res.username.split(' ').join('') + ".json";
+
+                        fs.writeFile(githubUsername, JSON.stringify(res, null, '\t'), function (err) {
+
+                            if (err) {
+                                //=========== logs an error if incorrect
+                                return console.log(err);
+                            }
+
+
+
+                            //=============== If correct, we will do an axios request to find information on this user.
+                            const queryUrl = "https://api.github.com/users/" + res.username;
+                            const queryStarUrl = "https://api.github.com/users/" + res.username + "/starred";
+
+
+                        });
