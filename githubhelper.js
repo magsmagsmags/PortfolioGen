9dcbb60cf24f3d0c7470f77fc704e000fb3ac6b1
Maggie Kiefer <57575667+magsmagsmags@users.noreply.github.com>
2020-01-17 23:49:20 -0600

    indexjs chages to streamline and major readme revisions

---
+++ githubhelper.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+
+function GeitHubHelper() {}
+
+GeitHubHelper.prototype,getUserInfo = function(username){
+    const someUrl = "https://api.github.com/users/" + username;
+    return axios.get(someUrl);
+}
+
+
+module.exports = GitHubHelper;
