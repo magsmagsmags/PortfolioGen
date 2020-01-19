459459265c3eedc90e0fc327d8e8f2a4af6a4c97
Maggie Kiefer <57575667+magsmagsmags@users.noreply.github.com>
2020-01-18 21:28:35 -0600

    made it work but very sloppy

--- githubhelper.js
+++ githubhelper.js
@@ -1,13 +1,59 @@
-const axios = require("axios"); /// .default?
+// const axios = require("axios"); /// .default?
+// const HTMLToPDF = require("html-pdf");
+// const generate = require("./generateHTML");
 
-function GithubHelper() {}
+// const starredURL = "https://api.github.com/users/${userName}/starred";
 
-GithubHelper.prototype.getUserInfo = function (username) {
-    const someUrl = "https://api.github.com/users/" + username;
-    return axios.get(someUrl);
-}
 
-const gh = new GithubHelper();
-gh.getUserInfo("magsmagsmags").then(response => console.log(response.data));
+// function GithubHelper() {}
 
-module.exports = GithubHelper;
+// GithubHelper.prototype.getUserInfo = function (userName) {
+//     const someUrl = "https://api.github.com/users/" + userName;
+//     return axios.get(someUrl);
+// }
+
+// HTMLToPDF.create(generate(res, starResponse, userData), options).toFile(`./${userName}.pdf`, function (err, response) {
+//     if (err) return console.log(err);
+//     console.log(response);
+// });
+// // });
+// // });
+// // });
+
+// function GithubHelper(someUrl) {
+//     return axios.get(someUrl)
+//         .then(function (gitResponse) {
+
+//             let userData = {
+//                 proPic: (gitResponse.data.avatar_url + ".png"),
+
+//                 fullName: (gitResponse.data.name),
+
+//                 gitUsername: (gitResponse.data.login),
+
+//                 location: (gitResponse.data.location),
+
+//                 profileURL: (gitResponse.data.html_url),
+
+//                 blog: (gitResponse.data.blog),
+
+//                 userBio: (gitResponse.data.bio),
+
+//                 publicRepos: (gitResponse.data.public_repos),
+
+//                 followers: (gitResponse.data.followers),
+
+//                 following: (gitResponse.data.following)
+//             };
+
+//             return userData;
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+// };
+
+// const gh = new GithubHelper();
+// gh.getUserInfo(res.userName).then(ghResponse => console.log(ghResponse.data));
+
+// module.exports = GithubHelper;
--- index.js
+++ index.js
@@ -1,66 +1,124 @@
 const generateHTML = require("./generateHTML.js");
-const GithubHelper = require("./githubhelper.js");
-const fs = require("fs");
-const inquirer = require("inquirer");
-// generator
-//// var pdf = require("html-pdf");
+// const GithubHelper = require("./githubhelper");
 
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios").default;
+// const HTMLToPDF = require("html-pdf");
+// const generate = require("./generateHTML");
 
 // moved to githubhelper.js: const axios = require('axios').default;
-// const querystring = require('querystring'); ///// ???
+const HTMLToPDF = require("html-pdf");
+const generate = require("./generateHTML");
 
 
-
-const questions = [{
-        type: "input",
-        name: "name",
-        message: "What is your name, my friend?"
-    },
-    {
-        type: "input",
-        name: "username",
-        message: "What is your GitHub Username?"
-    },
-    {
-        type: "rawlist",
-        name: "color",
-        message: "Which color do you like best?",
-        choices: [
-            "green",
-            "blue",
-            "pink",
-            "red"
-        ]
-    }
-];
-
-const helper = new GithubHelper();
+// const helper = new GithubHelper();
 
 // function writeToFile(fileName, data) {
-
-// };
+// }; don't need? 
 
 
 function init() {
     //getUserInfo is asyncronous bc we use axios for a network call to return a promise
-    inquirer.prompt(questions).then(response => {
-        helper.getUserInfo(response.username).then(ghResponse => {
-            const fileHTML = generateHTML({
-                name: response.name,
-                followers: ghResponse.data.followers,
-                // ghResponse defined? check. 
-                color: "pink"
-            });
-            console.log(fileHTML);
-            fs.writeFile("profile.html", fileHTML, function (err) {
-                console.log(err);
+    inquirer.prompt([{
+            type: "input",
+            name: "name",
+            message: "What is your name, my friend?"
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "What is your GitHub Username?"
+        },
+        {
+            type: "rawlist",
+            name: "color",
+            message: "Which color do you like best?",
+            choices: [
+                "green",
+                "blue",
+                "pink",
+                "red"
+            ]
+        }
+    ]).then(function (res) {
+        const nickname = res.name;
+        const userName = res.username;
+        const userColor = res.color;
+        const queryURL = `https://api.github.com/users/${userName}`;
+        const starredURL = `https://api.github.com/users/${userName}/starred`;
+        console.log(userName, userColor, nickname);
 
-                //then generate html to pdf
-                // keeping checking against your demo html 
-            })
-        })
-    }) // response paren and curly
+        gitHubRequest(queryURL).then(function (userData) {
+            gitHubStars(starredURL).then(function (starResponse) {
+                const options = {
+                    format: 'Letter'
+                };
+                fs.writeFile("profile.html", generateHTML, function (err) {
+                    console.log(err);
+                    HTMLToPDF.create(generate(res, starResponse, userData), options).toFile(`./${userName}.pdf`, function (err, response) {
+                        if (err) return console.log(err);
+                        console.log(response);
+                    });
+                });
+            });
+        });
+
+        function gitHubRequest(queryURL) {
+            return axios.get(queryURL)
+                .then(function (gitResponse) {
+
+                    let userData = {
+                        proPic: (gitResponse.data.avatar_url + ".png"),
+
+                        fullName: (gitResponse.data.name),
+
+                        gitUsername: (gitResponse.data.login),
+
+                        location: (gitResponse.data.location),
+
+                        profileURL: (gitResponse.data.html_url),
+
+                        blog: (gitResponse.data.blog),
+
+                        userBio: (gitResponse.data.bio),
+
+                        publicRepos: (gitResponse.data.public_repos),
+
+                        followers: (gitResponse.data.followers),
+
+                        following: (gitResponse.data.following)
+                    };
+
+                    return userData;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        };
+
+        function gitHubStars(starredURL) {
+            return axios.get(starredURL)
+                .then(function (starResponse) {
+                    return starResponse.data.length;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        };
+    })
 }; //init closing curly brace
 
 
 init();
+
+// fs.writeFile("profile.html", fileHTML, function (err) {
+//     console.log(err);
+
+//     //then generate html to pdf
+//     // keeping checking against your demo html 
+// })
+// })
+// .catch(error => {
+// console.error('Rejected function called: ' + error.message);
+// })
